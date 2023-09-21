4518a5f52a6a5dfcdda247dcd7c73b7c6f82452d
Toby16 <bolajibaasit14@gmail.com>
2023-09-21 18:59:13 -0400

    Changes come from within | Report: Improved efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,50 +12,39 @@ to meet a given amount (total)
 
 def makeChange(coins, total):
     """
-    * Change comes from within
-    makeChange: Function to determine
-                the fewest number of coins needed
-                to meet a given amount (total)
+    Determine the fewest number of coins needed to make a given amount (total).
 
     Args:
-        coins - List of coins of different values (list)
-        total - given amount (int)
-    """
-    # If total is 0 or less, return 0
-    if total <= 0:
-        return 0
+        coins - List of coin values (list of ints)
+        total - Target amount (int)
 
-    # The value of a coin will always be an integer greater than 0
+    Returns:
+        The minimum number of coins needed to make the total.
+        If it's not possible to make the total with the given coins, return -1.
     """
-    # Gather all coins that are greater than 0
-    # and 'less then or equal to `total`'
-    """
-    if len(coins) != 0:
-        new_coins = [i for i in coins if ((i > 0) and (i <= total))]
+    if total < 0 or not coins:
+        return -1
 
-    """
-    Create a list to store the minimum number of coins needed
-    for each amount from 0 to the total.
-    """
+    # Initialize a list
+    # to store the minimum number of coins for each amount from 0 to total.
     dp = [float('inf')] * (total + 1)
 
     # The minimum number of coins needed to make 0 is 0.
     dp[0] = 0
 
-    # Iterate through all coin values.
-    for coin in new_coins:
-        """
-        Update dp[i] if using this coin reduces the number of coins needed
-        to make amount i.
-        """
+    # Iterate through each coin value.
+    for coin in coins:
+        # Update dp[i] if using this coin reduces the number of coins needed
+        # to make amount i.
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    # The result is stored in dp[total].
-    if dp[total] != float('inf'):
-        return dp[total]
-    else:
+    # If dp[total] is still float('inf'),
+    # it means it's not possible to make the total with given coins.
+    if dp[total] == float('inf'):
         return -1
+
+    return dp[total]
 
 
 if __name__ == "__main__":
